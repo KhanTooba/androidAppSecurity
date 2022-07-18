2a84a083075334142a98f9a1c8e7a5e971e6fdeb
KhanTooba <jcs212245@iitd.ac.in>
2022-07-18 15:40:06 +0530

    added project.

---
+++ Calculator/app/src/main/java/com/example/new_sample/flyl.java
@@ -0,0 +1,11 @@
+package com.example.new_sample;
+
+import android.content.Context;
+
+public class flyl {
+    Context mContext;
+
+    public flyl(Context mContext) {
+        this.mContext = mContext;
+    }
+}
